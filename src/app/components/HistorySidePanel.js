d1f131f6104dd4c0e420dcea2984306efa0cb6f3
Jasim <jasimbinumar@gmail.com>
2024-12-02 22:38:47 +0500

    Create Conversation/History Side Panel

---
+++ src/app/components/HistorySidePanel.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HistorySidePanel = () => {
+  return (
+    <div className='border-2 border-secondary h-full w-56 m-5 mb-5 '>
+        <div className='border-2 border-secondary w-full h-10'>
+            <h1>
+                Side Panel Header
+            </h1>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default HistorySidePanel
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
 import Image from "next/image";
-
+import HistorySidePanel from "./components/HistorySidePanel";
 export default function LandingPage() {
   return (
     <div className="h-screen bg-primary items-center justify-center">
-      <div className="border-2 border-secondary bg-secondary">
-        <h1 className="text-primary text-lg text-center">
-          Hello World!
-        </h1>
-      </div>
+      <HistorySidePanel/>
     </div>
   );
 }
